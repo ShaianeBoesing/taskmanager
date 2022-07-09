e468db4a5ee561f3ef73dde56eacf80813464fe2
ShaianeBoesing <shaianeboesingrb@gmail.com>
2022-07-09 04:27:10 -0300

    :rocket: Migration - Criando tabela Tarefas

---
+++ database/migrations/2022_07_08_145921_criar_tabela_tarefas.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CriarTabelaTarefas extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tarefas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('projeto_id')->unsigned();
+            $table->foreign('projeto_id')->references('id')
+                                                 ->on('projetos')
+                                                 ->onDelete('cascade');;
+            $table->text('nome');
+            $table->timestamp('inicio')->useCurrent();
+            $table->timestamp('fim')->nullable();
+            $table->boolean('status')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tarefas');
+    }
+}
