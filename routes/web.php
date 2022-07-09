8cabbbc0805a44d9dc30c17ddf68da90cc4b33f2
ShaianeBoesing <shaianeboesingrb@gmail.com>
2022-07-09 04:28:49 -0300

    :rocket: Rotas

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProjetoController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [ProjetoController::class, 'index']);
+
+Route::prefix('/projeto')->group(function () {
+    Route::post('/novo', [ProjetoController::class, 'novo']);
+    Route::delete('/deletar/{id}', [ProjetoController::class, 'deletar']);
+    Route::put('/editar/{id}', [ProjetoController::class, 'editar']);
+});
+
+Route::prefix('/tarefa')->group(function () {
+    Route::post('/novo', [ProjetoController::class, 'novaTarefa']);
+    Route::delete('/deletar/{id}', [ProjetoController::class, 'deletarTarefa']);
+    Route::put('/editar/{id}', [ProjetoController::class, 'editarTarefa']);
+});
+
+
+
