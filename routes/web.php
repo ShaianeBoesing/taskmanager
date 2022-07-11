5dd532ff298779b9d61d9a0ab4d4e67f88a09863
ShaianeBoesing <shaianeboesingrb@gmail.com>
2022-07-10 21:48:35 -0300

    :rocket: adicionando rotas - para buscar tarefas pelo id do projeto - para retornar lista de projetos - para abrir formulario de projeto - any para enviar para tela principal quando receber rota inexistente

--- routes/web.php
+++ routes/web.php
@@ -13,19 +13,22 @@ use App\Http\Controllers\ProjetoController;
 |
 */
 
-Route::get('/', [ProjetoController::class, 'index']);
 
 Route::prefix('/projeto')->group(function () {
+    Route::get('/lista', [ProjetoController::class, 'listar'])->name('lista');
+    Route::get('/formulario/{id?}', [ProjetoController::class, 'formulario'])->name('formulario');
     Route::post('/novo', [ProjetoController::class, 'novo']);
     Route::delete('/deletar/{id}', [ProjetoController::class, 'deletar']);
     Route::put('/editar/{id}', [ProjetoController::class, 'editar']);
 });
 
 Route::prefix('/tarefa')->group(function () {
+    Route::get('/busca/{projeto_id}', [ProjetoController::class, 'buscarTarefas']);
     Route::post('/novo', [ProjetoController::class, 'novaTarefa']);
     Route::delete('/deletar/{id}', [ProjetoController::class, 'deletarTarefa']);
     Route::put('/editar/{id}', [ProjetoController::class, 'editarTarefa']);
 });
 
+Route::get('/{any}', [ProjetoController::class, 'index'])->where('any', '.*');
 
 
