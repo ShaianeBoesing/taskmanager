619ebf95836bad72b84f17513c7b55053f05f25f
ShaianeBoesing <shaianeboesingrb@gmail.com>
2022-07-10 21:23:47 -0300

    :rocket: Configuracoes do projeto

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
-require('./bootstrap');
+function notificacao(text, tipo){
+    $(".alert").addClass('alert-'+tipo)
+    $(".alert").text(text)
+    setTimeout(function (){
+        $(".alert").removeClass('alert-'+tipo)
+        $(".alert").empty()
+    }, 3000)
+
+}
